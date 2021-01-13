34f1d25676a48b18e501de3bf78e733ab133b5de
ale <thaianle88@gmail.com>
2021-01-13 20:48:48 +0700

    display categories dynamically with static data

--- main/tests.py
+++ main/tests.py
@@ -2,6 +2,7 @@ from django.urls import resolve
 from django.http import HttpRequest
 from django.test import TestCase
 from main.views import home_page
+from main.models import get_category
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -9,3 +10,15 @@ class HomePageTest(TestCase):
     def test_home_page_uses_correct_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
+
+
+class ListModelTest(TestCase):
+
+    def test_model_return_category(self):
+        response = get_category()
+        self.assertNotIsInstance(response, str)
+        try:
+            for res in response:
+                pass
+        except:
+            self.fail('Response should be iterable')
