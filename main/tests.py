bc8ca91839c897458dab70f12712a05c83278265
ale <thaianle88@gmail.com>
2021-01-13 09:36:48 +0700

    refactor the test of homepage

--- main/tests.py
+++ main/tests.py
@@ -4,24 +4,8 @@ from django.test import TestCase
 from main.views import home_page
 
 # Create your tests here.
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_can_resolve_for_root(self):
-        found = resolve("/")
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_return_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        content = response.content.decode("utf-8")
-        self.assertTrue(content.startswith("<html>"))
-        self.assertIn("<title>eCom-Store</title>", content)
-
-    def test_home_page_show_list_of_category(self):
-        request = HttpRequest()
-        response = home_page(request)
-        content = response.content.decode("utf-8")
-        self.assertIn('id="product_category"', content)
-        self.assertIn('<li>Food</li>', content)
-        self.assertIn('<li>Household</li>', content)
-        self.assertIn('<li>Computer</li>', content)
+    def test_home_page_uses_correct_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
